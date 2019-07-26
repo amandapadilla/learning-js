685045212080eb5e0ab71506a727f5f6b2e4620f
amandapadilla <amandapadillasantiago@gmail.com>
2019-07-26 16:49:21 +0200

    make arrays ex-1

---
+++ repaso/arrays/arrays.js
@@ -0,0 +1,11 @@
+"use strict";
+debugger;
+
+function workWithMovies() {
+  const arr = ["Los amantes del Círculo Polar", "Delicatessen", "ET"];
+  arr[3] = "Harry Potter";
+  arr[1] = "La teniente oNeil";
+  console.log(arr);
+}
+
+workWithMovies();
