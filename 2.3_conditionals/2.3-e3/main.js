214666e2aeb862db0a6ab3329468ad71c8794d77
amandapadilla <amandapadillasantiago@gmail.com>
2019-08-18 22:47:15 +0200

    upload conditionals exercises

---
+++ 2.3_conditionals/2.3-e3/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const number = 15;
+
+if (number - 15) {
+  console.log("El número es 0");
+} else if (number - 20) {
+  console.log("El número es negativo");
+} else if (number + 2) {
+  console.log("El número más 2 es: mayor 13 pero menor o igual que 20");
+} else if (number * 2) {
+  console.log("El número es mayor que 20 pero menor que 50");
+} else {
+  console.log("el número no es 123123125");
+}
---
+++ 2.3_conditionals/2.3-e4/main.js
@@ -0,0 +1,12 @@
+// Conversor de edad de perro a humano
+// Las reglas son las siguientes:
+// El primer año de un perro equivale a 15 años de humano
+// El segundo año de un perro equivale a nueve años de humano
+// A partir del tercero, cada año de perro equivale a 5 años de humano.
+
+const firstYear = 15;
+const secondYear = 9;
+const plusThird = 5;
+let dogAge;
+
+if ()
